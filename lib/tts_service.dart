b873bba829e61f5eb3b4cc745a5f0950de0b3132
FISohan <fisohan7@gmail.com>
2025-09-29 12:35:09 +0600

    fix markdown text to speech

--- lib/tts_service.dart
+++ lib/tts_service.dart
@@ -15,10 +15,30 @@ class TtsService {
     _flutterTts.setSpeechRate(1.0);
   }
 
+  String _normalizeMarkdown(String markdownText) {
+    // Remove headers
+    String text = markdownText.replaceAll(RegExp(r'^#+\s+', multiLine: true), '');
+    // Remove bold and italics
+    text = text.replaceAll(RegExp(r'\*{1,2}(.*?)\*{1,2}'), r'$1');
+    // Remove list markers
+    text = text.replaceAll(RegExp(r'^\s*[\*\-]\s+', multiLine: true), '');
+    text = text.replaceAll(RegExp(r'^\s*\d+\.\s+', multiLine: true), '');
+    // Remove links, keeping the text
+    text = text.replaceAll(RegExp(r'\[([^\]]+)\]\([^\)]+\)'), r'$1');
+    // Remove blockquotes
+    text = text.replaceAll(RegExp(r'^>\s*', multiLine: true), '');
+    // Remove horizontal rules
+    text = text.replaceAll(RegExp(r'^\s*[-*_]{3,}\s*$', multiLine: true), '');
+    // Remove empty lines
+    text = text.split('\n').map((line) => line.trim()).where((line) => line.isNotEmpty).join('\n');
+    return text;
+  }
+
   Future<void> speak(String text) async {
-    if (text.isNotEmpty) {
+    final normalizedText = _normalizeMarkdown(text);
+    if (normalizedText.isNotEmpty) {
       isSpeaking.value = true;
-      await _flutterTts.speak(text);
+      await _flutterTts.speak(normalizedText);
     }
   }
 
